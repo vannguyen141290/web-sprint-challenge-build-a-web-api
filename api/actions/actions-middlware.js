2c9ea5c5ec502d99d16698d5c369562e348ca854
thi <vannguyen141290@gmail.com>
2021-10-01 03:14:50 -0700

    wip

--- api/actions/actions-middlware.js
+++ api/actions/actions-middlware.js
@@ -1,1 +1,35 @@
-// add middlewares here related to actions
+const actionSchema = require('../../services/actionSchema')
+const Action = require('./actions-model')
+
+function validateId(req, res, next) {
+    Action.get(req.params.id)
+        .then(result => {
+            if(result) {
+                req.found = result
+                next()
+            } else {
+                next({
+                    status: 404,
+                    message: 'Id Not Found!'
+                })
+            }
+        })
+        .catch(next)
+}
+
+// async function validateProject(req, res, next){
+//     try {
+//         const validated = await projectSchema.validate(req.body)
+//         req.body = validated
+//         next()
+//     } catch (err) {
+//         next({
+//             status: 400,
+//             message: err.message
+//         })
+//     }
+// }
+
+module.exports = {
+    validateId,
+}
--- api/actions/actions-router.js
+++ api/actions/actions-router.js
@@ -1,15 +1,16 @@
 const express = require('express')
 const Action = require('./actions-model')
+const { validateId } = require('./actions-middlware')
 
 const router = express.Router()
 
-// router.get('/', (req, res, next) => {
+router.get('/', validateId, (req, res) => {
+    res.status(200).json(req.found)
+})
 
-// })
-
-// router.get('/', (req, res, next) => {
-
-// })
+router.get('/:id', validateId, (req, res) => {
+    res.status(200).json(req.found)
+})
 
 // router.get('/', (req, res, next) => {
 
